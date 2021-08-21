09dcd01a457f6e765fc59e543331e980216444f7
yaw boakye-owusu <yaw.owusu@hotmail.com>
2021-08-22 00:57:02 +0100

    created add two numbers logic

--- src/me/boakye/Main.java
+++ src/me/boakye/Main.java
@@ -1,8 +1,17 @@
 package me.boakye;
 
+import me.boakye.lib.Calculator;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Calculator calculator = new Calculator();
+
+        // adding two numbers
+        int firstToAdd = 5;
+        int secondToAdd = 7;
+        System.out.printf("%d + %d = %d%n", firstToAdd, secondToAdd, calculator.addTwoNumber(firstToAdd, secondToAdd));
+
     }
 }
