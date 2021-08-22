81723e7674cd118fabadb2f5b461be555b56e04c
yaw boakye-owusu <yaw.owusu@hotmail.com>
2021-08-22 01:00:09 +0100

    divide numbers

--- src/me/boakye/Main.java
+++ src/me/boakye/Main.java
@@ -13,5 +13,11 @@ public class Main {
         int secondToAdd = 7;
         System.out.printf("%d + %d = %d%n", firstToAdd, secondToAdd, calculator.addTwoNumber(firstToAdd, secondToAdd));
 
+        // divideTwoNumbers
+        double firstToDivide = 13;
+        double secondToDivide = 4;
+        System.out.printf("%f / %f = %f%n", firstToDivide, secondToDivide, calculator.divideTwoNumbers(firstToDivide, secondToDivide));
+
+
     }
 }
